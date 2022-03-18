8086876782f1a340295d8d3e1b8b357325a5f37f
Zzynn1224 <99325673+Zzynn1224@users.noreply.github.com>
2022-03-18 08:48:04 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -61,8 +61,7 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Report Group", "https://t.me/+MwgSi5vmQEA2N2Vk")
+    buttons.buildbutton("Owner", "https://t.me/KodokZumaIjo")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
