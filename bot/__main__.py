9259123b702d2394dad9f47374309f75cd04b8df
Zzynn1224 <99325673+Zzynn1224@users.noreply.github.com>
2022-03-20 16:27:04 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -66,9 +66,8 @@ def start(update, context):
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
-This bot can mirror all your links to Google Drive!
-Type /{BotCommands.HelpCommand} to get a list of available commands
-'''
+Hello bocah2 kontol:) 
+Saya adalah bot yang membagongkan ya:) nonton bowkep dulu aja:)'''
         sendMarkup(start_string, context.bot, update, reply_markup)
     else:
         sendMarkup('Not Authorized user, deploy your own mirror-leech bot', context.bot, update, reply_markup)
